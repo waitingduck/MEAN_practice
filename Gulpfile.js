2a558be5bdb882507da9de459907190bef46f53f
waitingduck <waitingduck@gmail.com>
2016-06-27 22:22:20 -0700

    add gulp watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -20,13 +20,13 @@ pipes.cleanDest = function () {
 
 pipes.builtPartialScripts = function() {
     return gulp.src(paths.partials)
-               .pipe(gulp.dest(paths.dest));
+        .pipe(gulp.dest(paths.dest));
 };
 
 pipes.builtAppStyles = function() {
     return gulp.src(paths.styles)
-               .pipe(plugins.concat('app.css'))
-               .pipe(gulp.dest(paths.dest));
+        .pipe(plugins.concat('app.css'))
+        .pipe(gulp.dest(paths.dest));
 };
 
 pipes.builtVendorStyles = function() {
@@ -37,19 +37,19 @@ pipes.builtVendorStyles = function() {
 
 pipes.builtVendorScripts = function() {
     return gulp.src(mainBowerFiles('**/*.js'))
-               .pipe(pipes.orderVendorScripts())
-               .pipe(plugins.concat('vendor.js'))
-               // .pipe(plugins.uglify())
-               .pipe(gulp.dest(paths.dest));
+        .pipe(pipes.orderVendorScripts())
+        .pipe(plugins.concat('vendor.js'))
+        // .pipe(plugins.uglify())
+        .pipe(gulp.dest(paths.dest));
 };
 
 //built app acripts
 pipes.builtAppScripts = function() {
     return gulp.src(paths.scripts)
-               .pipe(pipes.orderAppScripts())
-               .pipe(plugins.concat('app.js'))
-               .pipe(plugins.uglify())
-               .pipe(gulp.dest(paths.dest));
+        .pipe(pipes.orderAppScripts())
+        .pipe(plugins.concat('app.js'))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(paths.dest));
 };
 
 //order vendor scripts
@@ -65,28 +65,49 @@ pipes.orderAppScripts = function() {
 //copy bower-components into app folder
 pipes.bowerFiles = function() {
     return gulp.src(mainBowerFiles())
-               .pipe(gulp.dest(paths.dest + '/bower_components'));
+        .pipe(gulp.dest(paths.dest + '/bower_components'));
 };
 
 pipes.builtIndex = function() {
-  
+
     var vendorStyles = pipes.builtVendorStyles();
     var vendorScripts = pipes.builtVendorScripts();
     var appScripts = pipes.builtAppScripts();
     var appStyles = pipes.builtAppStyles();
     pipes.builtPartialScripts();
-    
+
     return gulp.src('app/index.html')
-          .pipe(gulp.dest(paths.dest))
-          .pipe(plugins.inject(vendorScripts, {relative:true, name:'bowerjs'}))
-          .pipe(plugins.inject(vendorStyles, {relative:true, name:'bowercss'}))
-          // .pipe(plugins.inject(gulp.src(mainBowerFiles()), {relative:true, name:'bower'}))
-          .pipe(plugins.inject(appScripts, {relative:true}))
-          .pipe(plugins.inject(appStyles, {relative:true}))
-          .pipe(gulp.dest(paths.dest));
+        .pipe(gulp.dest(paths.dest))
+        .pipe(plugins.inject(vendorScripts, {relative:true, name:'bowerjs'}))
+        .pipe(plugins.inject(vendorStyles, {relative:true, name:'bowercss'}))
+        // .pipe(plugins.inject(gulp.src(mainBowerFiles()), {relative:true, name:'bower'}))
+        .pipe(plugins.inject(appScripts, {relative:true}))
+        .pipe(plugins.inject(appStyles, {relative:true}))
+        .pipe(gulp.dest(paths.dest));
 };
 
 gulp.task('clean-app', pipes.cleanDest);
 gulp.task('build-app', ['clean-app'], pipes.builtIndex);
 
+gulp.task('build-app', ['clean-app'], pipes.builtIndex);
 
+
+gulp.task('watch', ['build-app'], function() {
+
+    gulp.watch(paths.index, function() {
+        return pipes.builtIndex();
+    });
+
+    gulp.watch(paths.scripts, function() {
+        return pipes.builtAppScripts();
+    });
+
+    gulp.watch(paths.styles, function() {
+        return pipes.builtAppStyles();
+    });
+
+    gulp.watch(paths.partials, function() {
+        return pipes.builtPartialScripts();
+    });
+
+});
